5ffc3c8e19e89ee6181118880d6555ce1c89d6f0
Umer Farooq <omar.faruk@outlook.com>
2021-11-13 19:04:44 +0000

    Reverted to Gird toolbar as it overflows

--- src/EditorToolbar/index.jsx
+++ src/EditorToolbar/index.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function EditorToolbar({ children, visible = true, ...rest }) {
     return (
-        <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          width: 'fit-content',
-          border: (theme) => `1px solid ${theme.palette.divider}`,
-          borderRadius: 1,
-          backgroundColor: 'background.paper',
-          color: 'text.secondary',
-          '& svg': {
-            m: 1.5,
-          },
-          '& hr': {
-            mx: 0.5,
-          },
-        }}
-      >
+        <Grid container 
+          alignItems="center"
+          sx={{
+            border: (theme) => `1px solid ${theme.palette.divider}`,
+            borderRadius: 1,
+            backgroundColor: 'background.paper',
+            color: 'text.secondary',
+            '& svg': {
+              m: 1.5,
+            },
+            '& hr': {
+              mx: 0.5,
+            },
+          }}
+        >
             {children}
-        </Box>
+        </Grid>
     );
 }
 
